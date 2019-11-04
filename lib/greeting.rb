1b16c6366eb0eb57dac5d12e3be080859df63370
Muminjon Abduraimov <muminjonguru@outlook.com>
2019-11-04 13:25:44 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,9 @@
 # code the #greeting method here!
 puts "Hi! I'm Muminjon, What's your name?"
 ism = gets.chomp
+greeting(ism)
 
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
+
